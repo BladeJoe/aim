d34d98b402b3d7119a7e31da75a0ad16e418229a
BladeJoe <bladejoe007@gmail.com>
2024-03-27 04:31:42 +0100

    ad

--- main.js
+++ main.js
@@ -1,9 +1,18 @@
-let target = document.querySelectorAll(".target")
+function getRandomPosition() {
+  var windowWidth = window.innerWidth;
+  var windowHeight = window.innerHeight;
+  var targetWidth = document.querySelector('.target').offsetWidth;
+  var targetHeight = document.querySelector('.target').offsetHeight;  
+  var maxX = windowWidth - targetWidth;
+  var maxY = windowHeight - targetHeight;  
+  var randomX = Math.floor(Math.random() * maxX);
+  var randomY = Math.floor(Math.random() * maxY);  
+  return { x: randomX, y: randomY };
+}
 
 function newPosition() {
-    randH = Math.random()
-    randV = Math.random()
-    target[0].style.top = (randV*800)+"px"
-    target[0].style.left = (randH*1800)+"px" 
-    console.log(target[0].style.left); 
+  var target = document.querySelector('.target');
+  var newPosition = getRandomPosition();
+  target.style.left = newPosition.x + 'px';
+  target.style.top = newPosition.y + 'px';
 }
